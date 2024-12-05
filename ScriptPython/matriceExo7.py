b004b70fb512627e89654cd3482c532cb904a420
shadowforce78 <planque.adam@gmail.com>
2024-12-05 15:10:36 +0100

    vault backup: 2024-12-05 15:10:35

---
+++ ScriptPython/matriceExo7.py
@@ -0,0 +1,9 @@
+# Soit les matrices : 
+import numpy as np
+
+M = np.matrix("0 1 0; 2 0 0; -2 0 1")
+M1 = np.matrix("2 0 0; 0 1 0; -2 0 1")
+M2 = np.matrix("1 0 0; 0 1 0; -2 0 1")
+I3 = np.ma("1 0 0; 0 1 0; 0 0 1")
+# Calculer P1, P2, P3
+# 
